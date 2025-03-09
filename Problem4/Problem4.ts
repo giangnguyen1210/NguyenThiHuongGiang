b56d14bd21f2a5357214bd54b696c6b2a05ec77a
hong.nt1 <giangnguyen.011210@gmail.com>
2025-03-09 15:33:26 +0700

    fix code Pr4

--- Problem4/Problem4.ts
+++ Problem4/Problem4.ts
@@ -1,14 +1,14 @@
 function sum_to_n_a(n: number): number {
 	// your code here
 	// Big O = O(1)
-	return  (n*(n+1))/2;
+	return (n * (n + 1)) / 2;
 }
 
 function sum_to_n_b(n: number): number {
 	// your code here
 	// Big O = O(n)
 	let sum = 0;
-	for(let i=1;i<n;i++){
+	for (let i = 1; i <= n; i++) {
 		sum += i;
 	}
 	return sum;
@@ -17,6 +17,7 @@ function sum_to_n_b(n: number): number {
 function sum_to_n_c(n: number): number {
 	// your code here
 	// Big O = O(n)
-	if (n ===1 ) return 1;
-	return n + sum_to_n_c(n-1);
+	if (n <= 0) return 0;
+	if (n === 1) return 1;
+	return n + sum_to_n_c(n - 1);
 }
